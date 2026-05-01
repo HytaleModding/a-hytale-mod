f3947a9900528e6236641fd055440d95acb9666b
ItsNeil17 <neil@hytalemodding.dev>
2026-05-01 23:31:52 +0530

    fix: get PlayerRef from component
    Signed-off-by: ItsNeil17 <neil@hytalemodding.dev>

--- src/main/java/dev/hytalemodding/events/ExampleEvent.java
+++ src/main/java/dev/hytalemodding/events/ExampleEvent.java
@@ -1,14 +1,19 @@
 package dev.hytalemodding.events;
 
+import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 public class ExampleEvent {
 
     public static void onPlayerReady(PlayerReadyEvent event) {
-        PlayerRef playerRef = event.getPlayer();
-        playerRef.sendMessage(Message.raw("Welcome " + playerRef.getDisplayName()));
+        Ref<EntityStore> ref = event.getPlayerRef();
+        PlayerRef playerRef = ref.getStore().getComponent(ref, PlayerRef.getComponentType());
+        assert playerRef != null;
+        playerRef.sendMessage(Message.raw("Welcome " + event.getPlayer().getDisplayName()));
         System.out.println("Someone joined the server!");
     }
 
