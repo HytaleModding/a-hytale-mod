b209c9e2ff35bbbc3596f26427d2f3257c8413b1
Alechilles <22alec22@gmail.com>
2026-05-02 09:19:59 -0400

    Remove Gradle manifest override

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,6 @@
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
-
 /**
  * NOTE: This is entirely optional and basics can be done in `settings.gradle.kts`
  */
-
-val hytaleServerVersion = "2026.03.26-89796e57b"
-
-tasks.named("generateManifest") {
-    doLast {
-        val manifestFile = file("src/main/resources/manifest.json")
-        @Suppress("UNCHECKED_CAST")
-        val manifest = JsonSlurper().parse(manifestFile) as MutableMap<String, Any?>
-        manifest["IncludesAssetPack"] = true
-        manifest["ServerVersion"] = hytaleServerVersion
-        val json = JsonOutput.prettyPrint(JsonOutput.toJson(manifest))
-            .replace(Regex("\\[\\s+]"), "[]")
-        manifestFile.writeText(json + System.lineSeparator())
-    }
-}
 
 repositories {
     // Any external repositories besides: MavenLocal, MavenCentral, HytaleMaven, and CurseMaven
